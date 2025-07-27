20d5ee7a62ae8bdd5e92d531d5453ed0d0f1adf8
Ivan <zaplatarivan@gmail.com>
2025-07-27 19:43:34 +0000

    simplified ObjDumpParse and removed it as a member from ObjDumpAddressMap effectively making it a static class

--- ObjDumpAddressMap.hpp
+++ ObjDumpAddressMap.hpp
@@ -5,6 +5,10 @@
 #include <sstream>
 #include <vector>
 #include <cctype>
+#include <tuple>
+#include <utility>
+
+#define u_map unordered_map
 
 struct AddressInfo {
     uint32_t lineNumber = 0;
@@ -33,82 +37,85 @@ bool try_stoul(const std::string& input, int base, uint64_t& output) {
     }
 }
 
+
+
 namespace std {
 
-    enum ParseState{
+    typedef tuple<u_map<uint64_t, AddressInfo>, u_map<string, uint32_t>, u_map<uint32_t, string>> ParseResult;
+
+    enum ParseState {
         FOUND_FILENAME,
         LOOKING,
     };
 
     class ObjDumpParser {
         public:
-        vector<string> tokenize_line(string line){
-            // split line by empty spaces
-            vector<string> tokens;
-                    
-            stringstream line_stream(line);
-            string token;
-            while(getline(line_stream, token, ' ')){
-                if(!token.empty()){
-                    tokens.push_back(token);
+            static vector<string> tokenize_line(string line){
+                // split line by empty spaces
+                vector<string> tokens;
+                        
+                stringstream line_stream(line);
+                string token;
+                while(getline(line_stream, token, ' ')){
+                    if(!token.empty()){
+                        tokens.push_back(token);
+                    }
                 }
+                return tokens;
             }
-            return tokens;
-        }
 
-        void parse(
-            string path,
-            unordered_map<uint64_t, AddressInfo>& addrMap,
-            unordered_map<string, uint32_t>& fileIDs,
-            unordered_map<uint32_t, string>& filePaths
-        ){
-            ifstream file(path);
-            
-            string line, current_filename;
-            uint32_t next_fileID = 0;
-            ParseState current_state = ParseState::LOOKING;
-            
-            while(getline(file, line)){
-                if(current_state == ParseState::LOOKING){
-                    if(line.empty()){
-                        continue;
-                    }
+            static ParseResult parse(string path){
 
-                    current_state = ParseState::FOUND_FILENAME;
+                ParseResult result;
 
-                    // split line by empty spaces
-                    vector<string> tokens = tokenize_line(line);
-                    if(!tokens.empty()){
-                        current_filename = tokens[0];
-                    }
-                } else {
-                    if(line.empty()){
-                        current_state = ParseState::LOOKING;
-                    } else {
+                ifstream file(path);
+                
+                string line, current_filename;
+                uint32_t next_fileID = 0;
+                ParseState current_state = ParseState::LOOKING;
+                
+                while(getline(file, line)){
+                    if(current_state == ParseState::LOOKING){
+                        if(line.empty()){
+                            continue;
+                        }
+
+                        current_state = ParseState::FOUND_FILENAME;
+
+                        // split line by empty spaces
                         vector<string> tokens = tokenize_line(line);
-                        uint32_t line_number = 0;
-                        uint64_t address = 0xdeadbeeful;
-                        if(tokens.size() >= 3 && try_stoui(tokens[1], 10, line_number) && try_stoul(tokens[2], 16, address)){
-                            addrMap[address] = AddressInfo{line_number, next_fileID++};
-                            filePaths[addrMap[address].fileID] = current_filename;
-                            fileIDs[tokens[0]] = addrMap[address].fileID;
+                        if(!tokens.empty()){
+                            current_filename = tokens[0];
+                        }
+                    } else {
+                        if(line.empty()){
+                            current_state = ParseState::LOOKING;
+                        } else {
+                            vector<string> tokens = tokenize_line(line);
+                            uint32_t line_number = 0;
+                            uint64_t address = 0xdeadbeeful;
+                            if(tokens.size() >= 3 && try_stoui(tokens[1], 10, line_number) && try_stoul(tokens[2], 16, address)){
+                                get<0>(result)[address] = AddressInfo{line_number, next_fileID++};
+                                get<1>(result)[current_filename] = get<0>(result)[address].fileID;
+                                get<2>(result)[get<0>(result)[address].fileID] = current_filename;
+                            }
                         }
                     }
                 }
+                return result;
             }
-        }
     };
 
     class ObjDumpAddressMap {
         private:
-            ObjDumpParser p;
             string dumpPath;
             unordered_map<uint64_t, AddressInfo> addrMap;
             unordered_map<string, uint32_t> fileIDs;
             unordered_map<uint32_t, string> filePaths;
         public:
             ObjDumpAddressMap(string path) : dumpPath(path){
-                p.parse(path, addrMap, fileIDs, filePaths);
+                auto result = ObjDumpParser::parse(path);
+                tie(addrMap, fileIDs, filePaths) = move(result);
             }
 
             uint32_t get_pathID(string path){
@@ -117,10 +124,6 @@ namespace std {
 
             string get_path(uint32_t fileID){
                 return filePaths[fileID];
-            }
-
-            const unordered_map<uint64_t, AddressInfo>& get_addr_map(){
-                return addrMap;
             }
 
             void set_pathID(string path, uint32_t value){
@@ -135,12 +138,9 @@ namespace std {
                 return addrMap[addr];
             }
 
-            string get_dump_path(){
-                return dumpPath;
-            }
+            auto begin() const { return addrMap.begin(); }
+
+            auto end() const { return addrMap.end(); }
     };
-
-    
-
 }
 
